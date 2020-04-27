df9d5d11a43d0a1459d42179412116f38ce32454
blopez486 <63551554+blopez486@users.noreply.github.com>
2020-04-26 21:08:24 -0400

    Add files via upload

---
+++ Module_03/connect-mysql.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    DEFINE ('$username', 'blopez486');
+    DEFINE ('$password', 'Legend13!');
+    DEFINE ('$servername', 'localhost');
+    DEFINE ('$db_name', 'sample_db');
+
+
+// Make a MySQL Connection
+
+$con= "mysqli_connect"($servername, $username, $password, $db_name);
+
+?>
